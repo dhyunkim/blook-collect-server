fddda14309278fdc4110f2e5bc3de098821718ea
dhyunkim <imzan@naver.com>
2022-05-29 02:26:52 +0900

    config 추가 및 port 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import * as config from 'config';
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(config.get('port'));
 }
 bootstrap();
